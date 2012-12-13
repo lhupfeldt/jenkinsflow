93ad99f1c24d4bb2c756350060a45a232916b95a
Lars Hupfeldt Nielsen <lhn@ole2.(none)>
2012-12-13 01:28:30 +0100

    Simple demo of parallel an serial

---
+++ demo/buildsome.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+# NOTE: To run the demo you must have the following jobs defined in jenkins/hudson
+# quick
+# quick_fail
+# wait10
+# wait10_fail
+# wait5
+# wait5_fail
+
+import sys
+import os.path
+from os.path import join as jp
+here = os.path.dirname(__file__)
+sys.path.append(jp(here, '../..'))
+
+import logging
+
+from jenkinsapi import jenkins
+
+from unbuffered import UnBuffered
+sys.stdout = UnBuffered(sys.stdout)
+
+from jenkinsflow.jobcontrol import parallel, serial
+
+jenkinsurl = "http://localhost:8080"
+
+def main():
+    logging.basicConfig()
+    logging.getLogger("").setLevel(logging.INFO)
+
+    api = jenkins.Jenkins(jenkinsurl)
+
+    with serial(api, timeout=15, report_interval=3) as ctrl:
+        ctrl.invoke('quick')
+        ctrl.invoke('wait10')
+        ctrl.invoke('wait5')
+    
+    with parallel(api, timeout=20, report_interval=3) as ctrl:
+        ctrl.invoke('quick')
+        ctrl.invoke('quick_fail')
+        ctrl.invoke('wait10')
+        ctrl.invoke('wait10_fail')
+        ctrl.invoke('wait5')
+        ctrl.invoke('wait5_fail')
+
+main()
---
+++ demo/unbuffered.py
@@ -0,0 +1,10 @@
+class UnBuffered:
+    def __init__ (self, stream):
+        self.stream = stream
+
+    def write(self, data):
+        self.stream.write(data)
+        self.stream.flush()
+
+    def __getattr__(self, attr):
+        return getattr(self.stream, attr)
