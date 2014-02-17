c8722504a5b99edfa9f3d777984ebcde39dc2da6
Lars Hupfeldt <lhn@hupfeldtit.dk>
2014-02-17 12:57:40 +0100

    Added test of UnBuffered class

---
+++ test/unbuffered_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+# Copyright (c) 2012 - 2014 Lars Hupfeldt Nielsen, Hupfeldt IT
+# All rights reserved. This work is under a BSD license, see LICENSE.TXT.
+
+import sys
+
+from framework import mock_api
+from jenkinsflow.jobcontrol import serial
+from jenkinsflow.unbuffered import UnBuffered
+
+
+def test_unbuffered():
+    sys.stdout = UnBuffered(sys.stdout)
+    with mock_api.api(__file__) as api:
+        api.job('unbuf', exec_time=0.5, max_fails=0, expect_invocations=1, expect_order=1)
+
+        with serial(api, timeout=70, job_name_prefix=api.job_name_prefix) as ctrl:
+            ctrl.invoke('unbuf')
--- unbuffered.py
+++ unbuffered.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2012 - 2014 Lars Hupfeldt Nielsen, Hupfeldt IT
 # All rights reserved. This work is under a BSD license, see LICENSE.TXT.
 
-class UnBuffered:
+class UnBuffered(object):
     """
     Implements unbuffered output.
     """
