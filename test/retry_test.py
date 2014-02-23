befd4e8537b43e76c6657b0f347824f55681c9ca
Lars Hupfeldt <lhn@hupfeldtit.dk>
2014-02-23 01:03:02 +0100

    Added another retry test

--- test/retry_test.py
+++ test/retry_test.py
@@ -3,8 +3,6 @@
 # Copyright (c) 2012 - 2014 Lars Hupfeldt Nielsen, Hupfeldt IT
 # All rights reserved. This work is under a BSD license, see LICENSE.TXT.
 
-from pytest import raises
-
 from jenkinsflow.flow import parallel, serial
 from framework import mock_api
 
@@ -82,12 +80,12 @@ def test_retry_through_parent_level():
                     ctrl3.invoke('j31_fail')
 
 
-def test_retry_through_outer_level():
+def test_retry_parallel_through_outer_level():
     with mock_api.api(__file__) as api:
         api.flow_job()
         api.job('j11', 0.1, max_fails=0, expect_invocations=1, expect_order=1)
         api.job('j21', 0.1, max_fails=0, expect_invocations=1, expect_order=2)
-        api.job('j31', 0.1, max_fails=0, expect_invocations=1, expect_order=2)
+        api.job('j31', 0.1, max_fails=0, expect_invocations=1, expect_order=3)
         api.job('j41_fail', 0.1, max_fails=2, expect_invocations=3, expect_order=3)
 
         with serial(api, timeout=70, job_name_prefix=api.job_name_prefix) as ctrl1:
@@ -98,6 +96,28 @@ def test_retry_through_outer_level():
                     ctrl3.invoke('j31')
                     with ctrl3.parallel(timeout=70, max_tries=2) as ctrl4:
                         ctrl4.invoke('j41_fail')
+
+
+def test_retry_serial_through_outer_level():
+    with mock_api.api(__file__) as api:
+        api.flow_job()
+        api.job('j11', 0.1, max_fails=0, expect_invocations=1, expect_order=1)
+        api.job('j21', 0.1, max_fails=0, expect_invocations=1, expect_order=2)
+        api.job('j31', 0.1, max_fails=0, expect_invocations=1, expect_order=3)
+        api.job('j41', 0.1, max_fails=0, expect_invocations=4, expect_order=3)
+        api.job('j42_fail', 0.1, max_fails=3, expect_invocations=4, expect_order=3)
+        api.job('j43', 0.1, max_fails=0, expect_invocations=1, expect_order=3)
+
+        with serial(api, timeout=70, job_name_prefix=api.job_name_prefix, max_tries=2) as ctrl1:
+            ctrl1.invoke('j11')
+            with ctrl1.serial(timeout=70) as ctrl2:
+                ctrl2.invoke('j21')
+                with ctrl2.parallel(timeout=70) as ctrl3:
+                    ctrl3.invoke('j31')
+                    with ctrl3.serial(timeout=70, max_tries=2) as ctrl4:
+                        ctrl4.invoke('j41')
+                        ctrl4.invoke('j42_fail')
+                        ctrl4.invoke('j43')
 
 
 def test_retry_mix():
