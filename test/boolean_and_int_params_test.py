dc8b3e6b2ba54bc1f636565ff0b0762275a816db
Lars Hupfeldt <lhn@hupfeldtit.dk>
2014-02-27 22:01:56 +0100

    Corrected parameter description

--- test/boolean_and_int_params_test.py
+++ test/boolean_and_int_params_test.py
@@ -11,7 +11,7 @@ def test_boolean_and_int_params():
     with mock_api.api(__file__) as api:
         api.flow_job()
         api.job('job-1', exec_time=0.5, max_fails=0, expect_invocations=1, expect_order=1, params=(('b1', False, 'boolean'), ('b2', True, 'boolean')))
-        api.job('job-2', exec_time=0.5, max_fails=0, expect_invocations=1, expect_order=2, params=(('i1', 1, 'boolean'), ('i2', 2, 'boolean')))
+        api.job('job-2', exec_time=0.5, max_fails=0, expect_invocations=1, expect_order=2, params=(('i1', 1, 'integer'), ('i2', 2, 'integer')))
 
         with serial(api, timeout=20, job_name_prefix=api.job_name_prefix, report_interval=1) as ctrl1:
             ctrl1.invoke('job-1', b1=True, b2=False)
