e22055a51e9892a4583299d6d2e5bd0e3efa47b5
Lars Hupfeldt <lhn@hupfeldtit.dk>
2014-03-01 15:12:01 +0100

    Copyright

--- test/framework/utils.py
+++ test/framework/utils.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2012 - 2014 Lars Hupfeldt Nielsen, Hupfeldt IT
+# All rights reserved. This work is under a BSD license, see LICENSE.TXT.
+
 import re
 
 _http_re = re.compile(r'https?://[^/]*/')
