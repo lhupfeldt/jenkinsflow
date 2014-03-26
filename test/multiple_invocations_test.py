8a534a9927ac0050b3182243c2b8bbf59127549e
Lars Hupfeldt <lhn@hupfeldtit.dk>
2014-03-26 17:33:52 +0100

    Test two flow invocations after each other

--- test/multiple_invocations_test.py
+++ test/multiple_invocations_test.py
@@ -5,7 +5,7 @@ from jenkinsflow.flow import serial
 from .framework import mock_api
 
 
-def test_multiple_invocations_immediate():
+def test_multiple_invocations_same_flow():
     with mock_api.api(__file__) as api:
         api.flow_job()
         _params = (('password', '', 'Some password'), ('s1', '', 'Some string argument'))
@@ -13,4 +13,17 @@ def test_multiple_invocations_immediate():
 
         with serial(api, timeout=70, job_name_prefix=api.job_name_prefix, report_interval=1) as ctrl1:
             ctrl1.invoke('j1', password='a', s1='b')
+            ctrl1.invoke('j1', password='something else', s1='asdasdasdasdad')
+
+
+def test_multiple_invocations_new_flow():
+    with mock_api.api(__file__) as api:
+        api.flow_job()
+        _params = (('password', '', 'Some password'), ('s1', '', 'Some string argument'))
+        api.job('j1', exec_time=0.01, max_fails=0, expect_invocations=2, expect_order=1, params=_params)
+
+        with serial(api, timeout=70, job_name_prefix=api.job_name_prefix, report_interval=1) as ctrl1:
+            ctrl1.invoke('j1', password='a', s1='b')
+
+        with serial(api, timeout=70, job_name_prefix=api.job_name_prefix, report_interval=1) as ctrl1:
             ctrl1.invoke('j1', password='something else', s1='asdasdasdasdad')
