ecbfdf01212784c2e389b9654337400fd3619822
Lars Hupfeldt <lhn@hupfeldtit.dk>
2014-08-10 00:54:01 +0200

    Minor cleanup

--- test/specialized_api_test.py
+++ test/specialized_api_test.py
@@ -1,14 +1,10 @@
 # Copyright (c) 2012 - 2014 Lars Hupfeldt Nielsen, Hupfeldt IT
 # All rights reserved. This work is under a BSD license, see LICENSE.TXT.
 
-import os
-
 from pytest import raises
 
 from jenkinsflow import specialized_api
 
-
-here = os.path.abspath(os.path.dirname(__file__))
 
 def test_specialized_api_build_repr():
     class Job(object):
@@ -21,4 +17,3 @@ def test_specialized_api_init_api_no_password():
     with raises(Exception) as exinfo:
         specialized_api.Jenkins("dummy", "dummy", username="hugo")
     assert "You must specify both username and password or neither" in exinfo.value.message
-
