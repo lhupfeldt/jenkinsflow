d8d34ae3037ed4df0cca2b6bffbd73db8426c74d
Aleksey Maksimov <ctpeko3a@gmail.com>
2014-12-08 23:07:36 +0800

    Fixed problems with graph rendering with large json

--- visual/js/graph.js
+++ visual/js/graph.js
@@ -1,47 +1,52 @@
 window.onload=function(){
-    var nodePadding = 10;
- 
-    var links;
-    var nodes;
+    var links = [];
+    var nodes = [];
     var job_urls = []
- 
-    d3.json("http://localhost:9090/jenkinsflow/flow_graph.json", function(error, json) {
-          if (error) {
-              console.error(error.message);
-          } else {
-              links = json["links"];
-              nodes = json["nodes"];
-              job_urls =json["nodes"].map(function(s) {
-                return s.url;
-              });
-              update(links, nodes);
-          }
-      }
-    );
-    
-    var g = new dagreD3.graphlib.Graph().setGraph({
-        nodesep: 10,
-        ranksep: 70,
-        rankdir: "TD",
-        marginx: 20,
-        marginy: 20
-    });   
+    var svg = d3.select("svg");
 
-    nodes.forEach(function(node) {
-        g.setNode(node.id, {
-            label: node.name,
-            width: 100
-        });
+    d3.json("http://localhost:9090/jenkinsflow/flow_graph.json", function(error, json) {
+      if (error) {
+          console.error(error.message);
+      } else {
+          links = json["links"];
+          nodes = json["nodes"];
+          job_urls =json["nodes"].map(function(s) {
+            return s.url;
+          });
+          var dgrph = new dagreD3.graphlib.Graph().setGraph({
+              nodesep: 50,
+              ranksep: 70,
+              rankdir: "TD",
+              marginx: 20,
+              marginy: 20
+          });
+
+          nodes.forEach(function(node) {
+              // Calculate the size of the node
+              var text = svg.append("svg:text")
+                  .attr("x", "-200")
+                  .attr("y", "-200")
+                  .text(node.name)
+              var t_width = text.node().getBBox().width;
+              text.remove();
+
+              dgrph.setNode(node.id, {
+                  label: node.name,
+                  width: t_width
+              });
+          });
+
+          links.forEach(function(link) {
+              dgrph.setEdge(link.source, link.target, {
+                  arrowhead: "vee"
+              });
+          });
+
+          var render = new dagreD3.render();
+          var container = d3.select("svg g");
+
+          render(container, dgrph);
+        }
     });
-      
-    links.forEach(function(link) {
-        g.setEdge(link.source, link.target, {
-            arrowhead: "vee"
-        });
-    });
-      
-    var render = new dagreD3.render();
-    var container = d3.select("svg g");
-      
-    render(container, g);
+
 };
