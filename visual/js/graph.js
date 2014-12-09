8f4d9bde16a11a1f3b74f14e23f21ccb1cda894d
Aleksey Maksimov <aleksey.maksimov@sc.com>
2014-12-09 10:33:26 +0800

    Reformatted code and added svg resize

--- visual/js/graph.js
+++ visual/js/graph.js
@@ -1,8 +1,5 @@
 window.onload=function(){
-    var links = [];
-    var nodes = [];
     var job_urls = []
-    var svg = d3.select("svg");
 
     d3.json("http://localhost:9090/jenkinsflow/flow_graph.json", function(error, json) {
       if (error) {
@@ -13,40 +10,47 @@ window.onload=function(){
           job_urls =json["nodes"].map(function(s) {
             return s.url;
           });
-          var dgrph = new dagreD3.graphlib.Graph().setGraph({
-              nodesep: 50,
-              ranksep: 70,
-              rankdir: "TD",
-              marginx: 20,
-              marginy: 20
-          });
+          draw(json['nodes'], json["links"]);
+      }
+    });
+}
 
-          nodes.forEach(function(node) {
-              // Calculate the size of the node
-              var text = svg.append("svg:text")
-                  .attr("x", "-200")
-                  .attr("y", "-200")
-                  .text(node.name)
-              var t_width = text.node().getBBox().width;
-              text.remove();
-
-              dgrph.setNode(node.id, {
-                  label: node.name,
-                  width: t_width
-              });
-          });
-
-          links.forEach(function(link) {
-              dgrph.setEdge(link.source, link.target, {
-                  arrowhead: "vee"
-              });
-          });
-
-          var render = new dagreD3.render();
-          var container = d3.select("svg g");
-
-          render(container, dgrph);
-        }
+function draw(nodes, links) {
+    var svg = d3.select("svg");
+    var dgrph = new dagreD3.graphlib.Graph().setGraph({
+        nodesep: 50,
+        ranksep: 70,
+        rankdir: "TD",
+        marginx: 20,
+        marginy: 20
     });
 
-};
+    nodes.forEach(function(node) {
+        // Calculate the size of the node
+        var text = svg.append("svg:text")
+            .attr("x", "-200")
+            .attr("y", "-200")
+            .text(node.name)
+        var t_width = text.node().getBBox().width;
+        text.remove();
+
+        dgrph.setNode(node.id, {
+            label: node.name,
+            width: t_width
+        });
+    });
+
+    links.forEach(function(link) {
+        dgrph.setEdge(link.source, link.target, {
+            arrowhead: "vee"
+        });
+    });
+
+    var render = new dagreD3.render();
+    var container = d3.select("svg g");
+
+    render(container, dgrph);
+
+    svg.attr("height", g.graph().height + 40);
+    svg.attr("width", 2 * (g.graph().width) + 40);
+}
