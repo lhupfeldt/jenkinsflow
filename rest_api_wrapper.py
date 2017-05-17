863643b1c10ac7e0a2f4011250d883a83500b4ed
Lars Hupfeldt <lhn@hupfeldtit.dk>
2017-05-18 00:51:01 +0200

    Fix Python 3 BasicAuth - Why does requests require 'bytes' under Python 3 and 'unicode' under Python 2 ????

--- rest_api_wrapper.py
+++ rest_api_wrapper.py
@@ -1,3 +1,15 @@
+import sys
+
+major_version = sys.version_info.major
+
+
+if major_version >= 3:
+    def unicode(text, encoding):
+        # TODO: Why is this necessary???
+        # On python 2 we need to convert from bytes to unicode, but on python 3 we need the reverse!
+        return text.encode("utf-8")
+
+
 class ResourceNotFound(Exception):
     pass
 
