a0742935ba2c94499d6d8b7f82b34d9a9ba3f387
Lars Hupfeldt <lhn@hupfeldtit.dk>
2019-11-16 22:37:34 +0100

    Test: exclude py[cod] instead of just pyc.

--- test/tmp_install.sh
+++ test/tmp_install.sh
@@ -4,7 +4,7 @@ set -u
 source_dir=$(cd $(dirname $0)/.. && pwd)
 target_dir=$1
 
-rsync -a --delete --delete-excluded --exclude .git --exclude '*~' --exclude '*.pyc' --exclude '__pycache__' --exclude '*.cache' $source_dir/ $target_dir/
+rsync -a --delete --delete-excluded --exclude .git --exclude '*~' --exclude '*.py[cod]' --exclude '__pycache__' --exclude '*.cache' $source_dir/ $target_dir/
 mkdir -p $target_dir/.cache
 chmod -R a+r $target_dir
 chmod a+wrx $(find $target_dir -type d)
